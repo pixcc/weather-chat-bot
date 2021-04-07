7524441250dbb0139a815fbb68d5890e0edf3930
Ilya Shakhov <46408420+pixcc@users.noreply.github.com>
2021-04-07 06:24:42 +0500

    Add files via upload

--- src/bot.py
+++ src/bot.py
@@ -1,39 +1,34 @@
-from psycopg2.extensions import connection, cursor
-from telegram import Update, ReplyKeyboardRemove, KeyboardButton, ReplyKeyboardMarkup
-from telegram.ext import Updater, Dispatcher, CommandHandler, CallbackContext, MessageHandler, Filters, \
-    ConversationHandler, PicklePersistence
-from textwrap import dedent
-from typing import Dict, Any, List, Tuple, Optional
-
+import telegram
+import telegram.ext
+import textwrap
 import json
 import psycopg2
 import requests
 
-
 SET_LOCATION_MENU = 1
 
 
-def help_command(update: Update, context: CallbackContext) -> None:
-    help_message: str = dedent("""\
-                                /help - available commands
-                                /set_location - set a weather forecast location
-                                /get_weather - get a weather forecast
-                            """)
+def help_command(update, context):
+    help_message = textwrap.dedent("""\
+                                    /help - available commands
+                                    /set_location - set a weather forecast location
+                                    /get_weather - get a weather forecast
+                                """)
 
     context.bot.send_message(chat_id=update.effective_chat.id, text=help_message)
 
 
-def start(update: Update, context: CallbackContext) -> None:
-    start_message: str = dedent("""\
-                                Welcome to the weather bot!
-                                Here you can get a forecast for any location!
-                            """)
+def start(update, context):
+    start_message = textwrap.dedent("""\
+                                    Welcome to the weather bot!
+                                    Here you can get a forecast for any location!
+                                """)
 
     context.bot.send_message(chat_id=update.effective_chat.id, text=start_message)
     help_command(update, context)
 
 
-def open_db_connection() -> connection:
+def open_db_connection():
     return psycopg2.connect(
         database="database",
         user="postgres",
@@ -43,24 +38,22 @@ def open_db_connection() -> connection:
     )
 
 
-def get_location_from_db(user_id: int) -> Optional[Tuple[float, float]]:
+def get_location_from_db(user_id):
     with open_db_connection() as conn:
         with conn.cursor() as cur:
-            cur: cursor
-            conn: connection
             cur.execute(f"SELECT latitude, longitude FROM users WHERE id = {user_id}")
-            location: Optional[Tuple[float, float]] = cur.fetchone()
+            location = cur.fetchone()
     return location
 
 
-def current_location(update: Update, context: CallbackContext) -> int:
-    location_button: KeyboardButton = KeyboardButton(text="Send location", request_location=True)
-    cancel_button: KeyboardButton = KeyboardButton(text="Cancel")
-    keyboard: List[List[KeyboardButton]] = [[location_button], [cancel_button]]
-    reply_markup: ReplyKeyboardMarkup = ReplyKeyboardMarkup(keyboard, one_time_keyboard=True)
+def current_location(update, context):
+    location_button = telegram.KeyboardButton(text="Send location", request_location=True)
+    cancel_button = telegram.KeyboardButton(text="Cancel")
+    keyboard = [[location_button], [cancel_button]]
+    reply_markup = telegram.ReplyKeyboardMarkup(keyboard, one_time_keyboard=True)
 
-    user_id: int = update.effective_user.id
-    location: Optional[Tuple[float, float]] = get_location_from_db(user_id)
+    user_id = update.effective_user.id
+    location = get_location_from_db(user_id)
 
     if location is None:
         context.bot.send_message(chat_id=user_id, text="Your current location is not set")
@@ -72,82 +65,80 @@ def current_location(update: Update, context: CallbackContext) -> int:
     return SET_LOCATION_MENU
 
 
-def set_location(update: Update, context: CallbackContext) -> None:
+def set_location(update, context):
     with open_db_connection() as conn:
         with conn.cursor() as cur:
-            cur: cursor
-            conn: connection
-            latitude: float = update.message.location.latitude
-            longitude: float = update.message.location.longitude
+            latitude = update.message.location.latitude
+            longitude = update.message.location.longitude
             cur.execute(f"""INSERT INTO users VALUES({update.effective_user.id}, {latitude}, {longitude}) 
                             ON CONFLICT(id) DO UPDATE SET latitude = {latitude}, longitude = {longitude};""")
 
-    user_id: int = update.effective_user.id
-    message: str = "The new location is successfully set"
-    context.bot.send_message(chat_id=user_id, text=message, reply_markup=ReplyKeyboardRemove())
+    user_id = update.effective_user.id
+    message = "The new location is successfully set"
+    context.bot.send_message(chat_id=user_id, text=message, reply_markup=telegram.ReplyKeyboardRemove())
 
 
-def cancel(update: Update, context: CallbackContext) -> int:
-    return ConversationHandler.END
+def cancel(update, context):
+    return telegram.ext.ConversationHandler.END
 
 
-def get_weather(update: Update, context: CallbackContext) -> None:
-    user_id: int = update.effective_user.id
-    location: Optional[Tuple[float, float]] = get_location_from_db(user_id)
+def get_weather(update, context):
+    user_id = update.effective_user.id
+    location = get_location_from_db(user_id)
 
     if location is None:
-        message: str = dedent("""\
-                                Your current location is not set
-                                Please use this command /set_location
-                            """)
+        message = textwrap.dedent("""\
+                                    Your current location is not set
+                                    Please use this command /set_location
+                                """)
 
         context.bot.send_message(chat_id=user_id, text=message)
 
     else:
-        api_key: str = context.bot_data['weather_api_key']
-        latitude: float = location[0]
-        longitude: float = location[1]
+        api_key = context.bot_data['weather_api_key']
+        latitude = location[0]
+        longitude = location[1]
 
-        url: str = f"http://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={api_key}&units=metric"
-        response: requests.Response = requests.get(url)
+        url = f"http://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={api_key}&units=metric"
+        response = requests.get(url)
 
-        data: Dict[str, Any] = json.loads(response.text)
-        forecast: str = dedent(f"""\
-                                {data["weather"][0]["main"]}
+        data = json.loads(response.text)
+        forecast = textwrap.dedent(f"""\
+                                    {data["weather"][0]["main"]}
 
-                                Temp: {data["main"]["temp"]}°C
-                                Max temp: {data["main"]["temp_max"]}°C
-                                Min temp: {data["main"]["temp_min"]}°C
-                                Humidity: {data["main"]["humidity"]}%
-                                Wind speed: {data["wind"]["speed"]} m/s
-                            """)
+                                    Temp: {data["main"]["temp"]}°C
+                                    Max temp: {data["main"]["temp_max"]}°C
+                                    Min temp: {data["main"]["temp_min"]}°C
+                                    Humidity: {data["main"]["humidity"]}%
+                                    Wind speed: {data["wind"]["speed"]} m/s
+                                """)
 
         context.bot.send_message(chat_id=user_id, text=forecast)
 
 
-def main() -> None:
-    persistence: PicklePersistence = PicklePersistence(filename="pickle_data")
-    updater: Updater = Updater(token="token", persistence=persistence,
-                               use_context=True)
-    dispatcher: Dispatcher = updater.dispatcher
-    dispatcher.bot_data["weather_api_key"] = "key"
+def main():
+    persistence = telegram.ext.PicklePersistence(filename="pickle_data")
+    updater = telegram.ext.Updater(token="token", persistence=persistence,
+                                   use_context=True)
+    dispatcher = updater.dispatcher
+    dispatcher.bot_data["weather_api_key"] = "api_key"
 
-    conv_handler: ConversationHandler = ConversationHandler(
-        entry_points=[CommandHandler("set_location", current_location)],
+    conv_handler = telegram.ext.ConversationHandler(
+        entry_points=[telegram.ext.CommandHandler("set_location", current_location)],
         states={
             SET_LOCATION_MENU: [
-                MessageHandler(Filters.location, set_location),
+                telegram.ext.MessageHandler(telegram.ext.Filters.location, set_location),
             ],
         },
-        fallbacks=[MessageHandler(Filters.regex("^Cancel$"), cancel)],
+        fallbacks=[telegram.ext.MessageHandler(telegram.ext.Filters.regex("^Cancel$"), cancel)],
         persistent=True,
         name='conv_handler'
     )
 
     dispatcher.add_handler(conv_handler)
-    dispatcher.add_handler(CommandHandler("start", start))
-    dispatcher.add_handler(CommandHandler("help", help_command))
-    dispatcher.add_handler(CommandHandler("get_weather", get_weather))
+    dispatcher.add_handler(telegram.ext.CommandHandler("start", start))
+    dispatcher.add_handler(telegram.ext.CommandHandler("help", help_command))
+    dispatcher.add_handler(telegram.ext.CommandHandler("get_weather", get_weather))
 
     updater.start_polling()
     updater.idle()
